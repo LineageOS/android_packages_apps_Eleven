3db4a438621e0835d1b85924c46dca808b85b401
Alexander Martinz <amartinz@shiftphones.com>
2019-02-25 23:22:46 +0100

    Add gradient option for album art and use it by default
    Instead of blurring, extract the vibrant colors from the album art
    and apply it as gradient, where it transitions top to bottom
    from light to dark.
    
    As many people love blur, give them the option to toggle between them.
    
    Change-Id: I05d00c41db3e7dc6a8bfc6b1aa4972c6b594e7f9
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>
    Signed-off-by: Joey <joey@lineageos.org>

--- src/org/lineageos/eleven/widgets/AlbumScrimImage.java
+++ src/org/lineageos/eleven/widgets/AlbumScrimImage.java
@@ -1,24 +1,28 @@
 /*
-* Copyright (C) 2014 The CyanogenMod Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019 SHIFT GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.lineageos.eleven.widgets;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,16 +30,19 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import org.lineageos.eleven.R;
-import org.lineageos.eleven.cache.ImageFetcher;
 import org.lineageos.eleven.cache.ImageWorker;
 
-public class BlurScrimImage extends FrameLayout {
+import androidx.core.content.ContextCompat;
+
+public class AlbumScrimImage extends FrameLayout {
+    private static final int COLOR_GREY_30 = 0x4c000000;
+
     private ImageView mImageView;
-    private View mBlurScrim;
+    private View mScrimView;
 
     private boolean mUsingDefaultBlur;
 
-    public BlurScrimImage(Context context, AttributeSet attrs) {
+    public AlbumScrimImage(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mUsingDefaultBlur = true;
@@ -45,8 +52,8 @@ public class BlurScrimImage extends FrameLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
-        mImageView = (ImageView)findViewById(R.id.blurImage);
-        mBlurScrim = findViewById(R.id.blurScrim);
+        mImageView = findViewById(R.id.blurImage);
+        mScrimView = findViewById(R.id.blurScrim);
     }
 
     public ImageView getImageView() {
@@ -63,8 +70,8 @@ public class BlurScrimImage extends FrameLayout {
             return;
         }
 
-        Bitmap blurredBitmap = ((BitmapDrawable) getResources()
-                .getDrawable(R.drawable.default_artwork_blur)).getBitmap();
+        Bitmap blurredBitmap = ((BitmapDrawable) ContextCompat.getDrawable(
+                getContext(), R.drawable.default_artwork_blur)).getBitmap();
 
         TransitionDrawable imageTransition = ImageWorker.createImageTransitionDrawable(getResources(),
                 mImageView.getDrawable(), blurredBitmap, ImageWorker.FADE_IN_TIME_SLOW, true, true);
@@ -72,28 +79,29 @@ public class BlurScrimImage extends FrameLayout {
         TransitionDrawable paletteTransition = ImageWorker.createPaletteTransition(this,
                 Color.TRANSPARENT);
 
-
         setTransitionDrawable(imageTransition, paletteTransition);
         mUsingDefaultBlur = true;
     }
 
     /**
      * Sets the transition drawable
-     * @param imageTransition the transition for the imageview
+     *
+     * @param imageTransition   the transition for the imageview
      * @param paletteTransition the transition for the scrim overlay
      */
     public void setTransitionDrawable(TransitionDrawable imageTransition,
-                               TransitionDrawable paletteTransition) {
-        mBlurScrim.setBackground(paletteTransition);
+            TransitionDrawable paletteTransition) {
+        mScrimView.setBackground(paletteTransition);
         mImageView.setImageDrawable(imageTransition);
         mUsingDefaultBlur = false;
     }
 
-    /**
-     * Loads the current artwork into this BlurScrimImage
-     * @param imageFetcher an ImageFetcher instance
-     */
-    public void loadBlurImage(ImageFetcher imageFetcher) {
-        imageFetcher.loadCurrentBlurredArtwork(this);
+    public void setGradientDrawable(GradientDrawable gradientDrawable) {
+        // set scrim view to 30% grey to prevent full white layouts
+        final ColorDrawable scrimDrawable = new ColorDrawable(COLOR_GREY_30);
+        mScrimView.setBackground(scrimDrawable);
+
+        mImageView.setImageDrawable(gradientDrawable);
+        mUsingDefaultBlur = false;
     }
 }
