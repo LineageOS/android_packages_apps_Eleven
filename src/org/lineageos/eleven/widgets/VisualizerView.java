1ce362cf25217ecc4dbddf299478c96d1befaa08
Alexander Martinz <amartinz@shiftphones.com>
2019-02-25 19:38:44 +0100

    Start revamping the player UI
     * introduce ConstraintLayout as dependency
     * remove cardview at playback controls
     * replace next and previous drawables with skip drawables to match action
     * line up all buttons: shuffle - previous - play/pause - next - repeat
     * ripple side buttons (the buttons next to play/pause)
     * remove progress from play/pause button and decrease "ring" thickness
     * remove blinking current time while music is paused
    
    Change-Id: I224a6d293cd0f19e38b8355c7f47790f651125a3
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/eleven/widgets/VisualizerView.java
+++ src/org/lineageos/eleven/widgets/VisualizerView.java
@@ -23,6 +23,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.media.audiofx.Visualizer;
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -139,7 +140,7 @@ public class VisualizerView extends View {
     }
 
     public void initialize(Context context) {
-        mColor = context.getResources().getColor(R.color.visualizer_fill_color);
+        mColor = ContextCompat.getColor(context, R.color.visualizer_fill_color);
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
