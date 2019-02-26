6809c40751fabc239894b2090df7ee085a9f2502
Alexander Martinz <amartinz@shiftphones.com>
2019-02-26 18:31:06 +0100

    Get rid of icu4j dependency
    Change-Id: I0eb134110fc6116a89600b6d343d775da2f13530
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -45,37 +45,3 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 include $(BUILD_MULTI_PREBUILT)
-
-########################
-
-### FAKE RULES FOR ANDROID STUDIO SUPPORT
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := eleven-fake-icu4j
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(call all-java-files-under,../../../external/icu/android_icu4j/src/main/java)
-LOCAL_JAVA_RESOURCE_DIRS := $(LOCAL_PATH)/../../../external/icu/android_icu4j/resources
-LOCAL_JAVA_LANGUAGE_VERSION := 1.8
-include $(BUILD_JAVA_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := GenerateStudioFiles-Eleven
-LOCAL_MODULE_CLASS := FAKE
-LOCAL_MODULE_SUFFIX := -timestamp
-
-system_deps_icu4j := $(call java-lib-deps,eleven-fake-icu4j)
-
-system_libs_eleven_path := $(abspath $(LOCAL_PATH))/system_libs
-
-include $(BUILD_SYSTEM)/base_rules.mk
-
-.PHONY: copy_eleven_system_deps
-copy_eleven_system_deps: $(system_deps_icu4j)
-	$(hide) mkdir -p $(system_libs_eleven_path)
-	$(hide) rm -rf $(system_libs_eleven_path)/*.jar
-	$(hide) cp $(system_deps_icu4j) $(system_libs_eleven_path)/icu4j.jar
-
-$(LOCAL_BUILT_MODULE): copy_eleven_system_deps
-	$(hide) echo "Fake: $@"
-	$(hide) mkdir -p $(dir $@)
-	$(hide) touch $@
