55ebdd1bd319f16fa3a23c0e17c1f1a9820de633
Alexander Martinz <amartinz@shiftphones.com>
2019-02-26 17:20:46 +0100

    Add BuildConfig to allow using gradle compile time fields
    Change-Id: Id041904e356ee1e9205babbbbcaf31b2ffe9c3ef
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/eleven/ElevenApplication.java
+++ src/org/lineageos/eleven/ElevenApplication.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven;
@@ -18,33 +23,16 @@ import android.os.StrictMode;
 
 import org.lineageos.eleven.cache.ImageCache;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * Used to turn off logging for jaudiotagger and free up memory when
- * {@code #onLowMemory()} is called on pre-ICS devices. On post-ICS memory is
- * released within {@link ImageCache}.
- *
- * @author Andrew Neal (andrewdneal@gmail.com)
- */
 public class ElevenApplication extends Application {
-    private static final boolean DEBUG = false;
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onCreate() {
-        // Enable strict mode logging
-        enableStrictMode();
-        // Turn off logging for jaudiotagger.
-        Logger.getLogger("org.jaudiotagger").setLevel(Level.OFF);
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            enableStrictMode();
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onLowMemory() {
         ImageCache.getInstance(this).evictAll();
@@ -52,15 +40,15 @@ public class ElevenApplication extends Application {
     }
 
     private void enableStrictMode() {
-        if (DEBUG) {
-            final StrictMode.ThreadPolicy.Builder threadPolicyBuilder = new StrictMode.ThreadPolicy.Builder()
-                    .detectAll().penaltyLog();
-            final StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder()
-                    .detectAll().penaltyLog();
+        final StrictMode.ThreadPolicy.Builder threadPolicyBuilder = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyFlashScreen();
+        final StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
 
-            threadPolicyBuilder.penaltyFlashScreen();
-            StrictMode.setThreadPolicy(threadPolicyBuilder.build());
-            StrictMode.setVmPolicy(vmPolicyBuilder.build());
-        }
+        StrictMode.setThreadPolicy(threadPolicyBuilder.build());
+        StrictMode.setVmPolicy(vmPolicyBuilder.build());
     }
 }
