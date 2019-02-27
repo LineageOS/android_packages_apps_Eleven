52c48826e0fd2bf39460da0256d9e06e3a56f9c3
Alexander Martinz <amartinz@shiftphones.com>
2019-02-27 14:13:26 +0100

    menu: clean up playlist helpers
    Change-Id: I0f750f5a56bdcc1ee616ebc97168ba967c7bdc73
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/eleven/menu/RenamePlaylist.java
+++ src/org/lineageos/eleven/menu/RenamePlaylist.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven.menu;
@@ -16,24 +21,22 @@ package org.lineageos.eleven.menu;
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Audio;
+import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.TextUtils;
 
 import org.lineageos.eleven.R;
-import org.lineageos.eleven.format.Capitalize;
 import org.lineageos.eleven.utils.MusicUtils;
 
 /**
- * Alert dialog used to rename playlits.
+ * Alert dialog used to rename playlists.
  *
  * @author Andrew Neal (andrewdneal@gmail.com)
  */
 public class RenamePlaylist extends BasePlaylistDialog {
-
-    private String mOriginalName;
-
     private long mRenameId;
 
     /**
@@ -43,64 +46,57 @@ public class RenamePlaylist extends BasePlaylistDialog {
     public static RenamePlaylist getInstance(final Long id) {
         final RenamePlaylist frag = new RenamePlaylist();
         final Bundle args = new Bundle();
-        args.putLong("rename", id);
+        args.putLong(EXTRA_RENAME, id);
         frag.setArguments(args);
         return frag;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void onSaveInstanceState(final Bundle outcicle) {
-        outcicle.putString("defaultname", mPlaylist.getText().toString());
-        outcicle.putLong("rename", mRenameId);
+    public void onSaveInstanceState(@NonNull final Bundle outcicle) {
+        outcicle.putString(EXTRA_DEFAULT_NAME, mPlaylist.getText().toString());
+        outcicle.putLong(EXTRA_RENAME, mRenameId);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void initObjects(final Bundle savedInstanceState) {
-        mRenameId = savedInstanceState != null ? savedInstanceState.getLong("rename")
-                : getArguments().getLong("rename", -1);
-        mOriginalName = getPlaylistNameFromId(mRenameId);
-        mDefaultname = savedInstanceState != null ? savedInstanceState.getString("defaultname")
-                : mOriginalName;
-        if (mRenameId < 0 || mOriginalName == null || mDefaultname == null) {
+    public void initialize(final Bundle savedInstanceState) {
+        mRenameId = savedInstanceState != null
+                ? savedInstanceState.getLong(EXTRA_RENAME)
+                : getArguments().getLong(EXTRA_RENAME, -1);
+        final String originalName = MusicUtils.getNameForPlaylist(getContext(), mRenameId);
+        mDefaultName = savedInstanceState != null
+                ? savedInstanceState.getString(EXTRA_DEFAULT_NAME)
+                : originalName;
+        if (mRenameId < 0 || originalName == null || mDefaultName == null) {
             getDialog().dismiss();
             return;
         }
-        final String promptformat = getString(R.string.create_playlist_prompt);
-        mPrompt = String.format(promptformat, mOriginalName, mDefaultname);
+        mPrompt = getString(R.string.create_playlist_prompt);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onSaveClick() {
         final String playlistName = mPlaylist.getText().toString();
-        if (playlistName != null && playlistName.length() > 0) {
+        if (!TextUtils.isEmpty(playlistName)) {
             final ContentResolver resolver = getActivity().getContentResolver();
             final ContentValues values = new ContentValues(1);
-            values.put(Audio.Playlists.NAME, Capitalize.capitalize(playlistName));
+            values.put(Audio.Playlists.NAME, playlistName);
             resolver.update(Audio.Playlists.EXTERNAL_CONTENT_URI, values,
-                    MediaStore.Audio.Playlists._ID + "=?", new String[] {
-                        String.valueOf(mRenameId)
+                    MediaStore.Audio.Playlists._ID + "=?", new String[]{
+                            String.valueOf(mRenameId)
                     });
             getDialog().dismiss();
         }
     }
 
     @Override
-    public void onTextChangedListener() {
-        final String playlistName = mPlaylist.getText().toString();
+    public void afterTextChanged(Editable editable) {
         mSaveButton = mPlaylistDialog.getButton(Dialog.BUTTON_POSITIVE);
         if (mSaveButton == null) {
             return;
         }
-        if (playlistName.trim().length() == 0) {
+
+        final String playlistName = (editable == null ? "" : editable.toString().trim());
+        if (playlistName.length() == 0) {
             mSaveButton.setEnabled(false);
         } else {
             mSaveButton.setEnabled(true);
@@ -111,28 +107,4 @@ public class RenamePlaylist extends BasePlaylistDialog {
             }
         }
     }
-
-    /**
-     * @param id The Id of the playlist
-     * @return The name of the playlist
-     */
-    private String getPlaylistNameFromId(final long id) {
-        Cursor cursor = getActivity().getContentResolver().query(
-                MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, new String[] {
-                    MediaStore.Audio.Playlists.NAME
-                }, MediaStore.Audio.Playlists._ID + "=?", new String[] {
-                    String.valueOf(id)
-                }, MediaStore.Audio.Playlists.NAME);
-        String playlistName = null;
-        if (cursor != null) {
-            cursor.moveToFirst();
-            if (!cursor.isAfterLast()) {
-                playlistName = cursor.getString(0);
-            }
-        }
-        cursor.close();
-        cursor = null;
-        return playlistName;
-    }
-
 }
