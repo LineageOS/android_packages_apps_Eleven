7d2a7690c57dd97dadf970b76292c3f243995504
Alexander Martinz <amartinz@shiftphones.com>
2019-02-27 14:13:26 +0100

    menu: clean up playlist helpers
    Change-Id: I0f750f5a56bdcc1ee616ebc97168ba967c7bdc73
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/eleven/menu/BasePlaylistDialog.java
+++ src/org/lineageos/eleven/menu/BasePlaylistDialog.java
@@ -1,24 +1,26 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven.menu;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.WindowManager;
@@ -28,6 +30,7 @@ import android.widget.EditText;
 import org.lineageos.eleven.R;
 import org.lineageos.eleven.utils.MusicUtils;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 /**
@@ -35,7 +38,10 @@ import androidx.fragment.app.DialogFragment;
  *
  * @author Andrew Neal (andrewdneal@gmail.com)
  */
-public abstract class BasePlaylistDialog extends DialogFragment {
+public abstract class BasePlaylistDialog extends DialogFragment implements TextWatcher {
+    public static final String EXTRA_DEFAULT_NAME = "default_name";
+    public static final String EXTRA_PLAYLIST_LIST = "playlist_list";
+    public static final String EXTRA_RENAME = "rename";
 
     /* The actual dialog */
     protected AlertDialog mPlaylistDialog;
@@ -50,112 +56,68 @@ public abstract class BasePlaylistDialog extends DialogFragment {
     protected String mPrompt;
 
     /* The default edit text text */
-    protected String mDefaultname;
+    protected String mDefaultName;
 
-    /**
-     * {@inheritDoc}
-     */
+    @NonNull
     @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        // Initialize the alert dialog
+    public Dialog onCreateDialog(@NonNull final Bundle savedInstanceState) {
         mPlaylistDialog = new AlertDialog.Builder(getActivity()).create();
-        // Initialize the edit text
+        mPlaylistDialog.setButton(Dialog.BUTTON_POSITIVE, getString(R.string.save),
+                (dialog, which) -> {
+                    onSaveClick();
+                    MusicUtils.refresh();
+                    dialog.dismiss();
+                });
+        mPlaylistDialog.setButton(Dialog.BUTTON_NEGATIVE, getString(R.string.cancel),
+                (dialog, which) -> {
+                    MusicUtils.refresh();
+                    dialog.dismiss();
+                });
+
         mPlaylist = new EditText(getActivity());
-        // To show the "done" button on the soft keyboard
         mPlaylist.setSingleLine(true);
-        // All caps
         mPlaylist.setInputType(mPlaylist.getInputType() | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
                 | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
-        // Set the save button action
-        mPlaylistDialog.setButton(Dialog.BUTTON_POSITIVE, getString(R.string.save),
-                new OnClickListener() {
-
-                    @Override
-                    public void onClick(final DialogInterface dialog, final int which) {
-                        onSaveClick();
-                        MusicUtils.refresh();
-                        dialog.dismiss();
-                    }
-                });
-        // Set the cancel button action
-        mPlaylistDialog.setButton(Dialog.BUTTON_NEGATIVE, getString(R.string.cancel),
-                new OnClickListener() {
-
-                    @Override
-                    public void onClick(final DialogInterface dialog, final int which) {
-                        MusicUtils.refresh();
-                        dialog.dismiss();
-                    }
-                });
-
-        mPlaylist.post(new Runnable() {
-
-            @Override
-            public void run() {
-                // Request focus to the edit text
-                mPlaylist.requestFocus();
-                // Select the playlist name
-                mPlaylist.selectAll();
-            };
+        mPlaylist.post(() -> {
+            // Request focus to the edit text
+            mPlaylist.requestFocus();
+            // Select the playlist name
+            mPlaylist.selectAll();
         });
 
-        initObjects(savedInstanceState);
+        initialize(savedInstanceState);
+
+        mPlaylist.setText(mDefaultName);
+        mPlaylist.setSelection(mDefaultName.length());
+        mPlaylist.addTextChangedListener(this);
+
         mPlaylistDialog.setTitle(mPrompt);
         mPlaylistDialog.setView(mPlaylist);
-        mPlaylist.setText(mDefaultname);
-        mPlaylist.setSelection(mDefaultname.length());
-        mPlaylist.addTextChangedListener(mTextWatcher);
-        mPlaylistDialog.getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        mPlaylistDialog.getWindow()
+                .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         mPlaylistDialog.show();
         return mPlaylistDialog;
     }
 
-    /**
-     * Simple {@link TextWatcher}
-     */
-    private final TextWatcher mTextWatcher = new TextWatcher() {
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        /* Nothing to do */
+    }
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void onTextChanged(final CharSequence s, final int start, final int before,
-                final int count) {
-            onTextChangedListener();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void afterTextChanged(final Editable s) {
-            /* Nothing to do */
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void beforeTextChanged(final CharSequence s, final int start, final int count,
+    @Override
+    public void beforeTextChanged(final CharSequence s, final int start, final int count,
                 final int after) {
-            /* Nothing to do */
-        }
-    };
+        /* Nothing to do */
+    }
 
     /**
      * Initializes the prompt and default name
      */
-    public abstract void initObjects(Bundle savedInstanceState);
+    public abstract void initialize(Bundle savedInstanceState);
 
     /**
      * Called when the save button of our {@link AlertDialog} is pressed
      */
     public abstract void onSaveClick();
-
-    /**
-     * Called in our {@link TextWatcher} during a text change
-     */
-    public abstract void onTextChangedListener();
 
 }
