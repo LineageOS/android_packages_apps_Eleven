ec52a8c58721da4941c7ce5b2dd0da43a7178161
Alexander Martinz <amartinz@shiftphones.com>
2019-02-27 19:55:07 +0100

    Settings: convert to PreferenceFragmentCompat
    Change-Id: Ic5509377f06ff147180d3f58a58469afff3ee40f
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -24,6 +24,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v7-cardview \
     android-support-v7-palette \
+    android-support-v7-preference \
     android-support-v7-recyclerview \
     android-support-media-compat
 
