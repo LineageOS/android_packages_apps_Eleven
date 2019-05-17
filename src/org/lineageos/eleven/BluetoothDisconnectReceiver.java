7ffd5fe7bd8f6d40ed6f36950868967012c07e87
tufteddeer <tufteddeer@disroot.org>
2019-05-17 15:26:23 +0200

    Pause music on bluetooth audio device disconnect
    Ignores non-audio device disconnects
    
    Change-Id: I2c94a0a863104dcd51559441780e958a6de20186

---
+++ src/org/lineageos/eleven/BluetoothDisconnectReceiver.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.eleven;
+
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BluetoothDisconnectReceiver extends BroadcastReceiver {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "BluetoothDisconnectReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (DEBUG) Log.v(TAG, "Received intent: " + intent);
+
+        String action = intent.getAction();
+        if (action != null && action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+
+            if (intent.hasExtra("android.bluetooth.device.extra.DEVICE")) {
+                BluetoothDevice disconnectedDevice =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if (disconnectedDevice.getBluetoothClass().getMajorDeviceClass() == BluetoothClass.Device.Major.AUDIO_VIDEO) {
+                    Log.v(TAG, "audio device disconnected");
+                    // stop playback
+                    final Intent i = new Intent(context, MusicPlaybackService.class);
+                    i.setAction(MusicPlaybackService.SERVICECMD);
+                    i.putExtra(MusicPlaybackService.CMDNAME, MusicPlaybackService.CMDPAUSE);
+                    context.startForegroundService(i);
+                } else if (DEBUG) {
+                    Log.v(TAG, "Ignoring disconnect of bluetooth device with class " + disconnectedDevice.getBluetoothClass());
+                }
+            }
+        }
+    }
+}
