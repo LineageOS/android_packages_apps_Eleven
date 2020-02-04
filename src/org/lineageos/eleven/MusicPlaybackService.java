0fa7edb5e12d15a3e62a4b56febe6b6c7496b2c1
Danny Baumann <dannybaumann@web.de>
2020-02-04 09:42:27 +0100

    Refactor music playback service binding logic.
    Instead of using a static service reference managed in a distributed
    manner, track binding registrations and fetch service reference from
    registration.
    
    Change-Id: I59cf90b3024cf21310f44954cd7bf5d11e4899a5

--- src/org/lineageos/eleven/MusicPlaybackService.java
+++ src/org/lineageos/eleven/MusicPlaybackService.java
@@ -445,7 +445,7 @@ public class MusicPlaybackService extends Service {
     /**
      * Used to know when the service is active
      */
-    private boolean mServiceInUse = false;
+    private boolean mIsBound = false;
 
     /**
      * Used to know if something should be playing or not
@@ -568,8 +568,7 @@ public class MusicPlaybackService extends Service {
     @Override
     public IBinder onBind(final Intent intent) {
         if (D) Log.d(TAG, "Service bound, intent = " + intent);
-        cancelShutdown();
-        mServiceInUse = true;
+        mIsBound = true;
         return mBinder;
     }
 
@@ -579,7 +578,7 @@ public class MusicPlaybackService extends Service {
     @Override
     public boolean onUnbind(final Intent intent) {
         if (D) Log.d(TAG, "Service unbound");
-        mServiceInUse = false;
+        mIsBound = false;
         saveQueue(true);
 
         if (mReadGranted) {
@@ -594,11 +593,9 @@ public class MusicPlaybackService extends Service {
                 // Also delay stopping the service if we're transitioning between
                 // tracks.
             } else if (mPlaylist.size() > 0 || mPlayerHandler.hasMessages(TRACK_ENDED)) {
-                scheduleDelayedShutdown();
                 return true;
             }
         }
-        stopSelf(mServiceStartId);
 
         return true;
     }
@@ -608,8 +605,7 @@ public class MusicPlaybackService extends Service {
      */
     @Override
     public void onRebind(final Intent intent) {
-        cancelShutdown();
-        mServiceInUse = true;
+        mIsBound = true;
     }
 
     /**
@@ -706,9 +702,6 @@ public class MusicPlaybackService extends Service {
 
         mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         mShutdownIntent = PendingIntent.getService(this, 0, shutdownIntent, 0);
-
-        // Listen for the idle state
-        scheduleDelayedShutdown();
 
         // Bring the queue back
         reloadQueue();
@@ -839,21 +832,24 @@ public class MusicPlaybackService extends Service {
             if (SHUTDOWN.equals(action)) {
                 mShutdownScheduled = false;
                 releaseServiceUiAndStop();
-                return START_NOT_STICKY;
             }
 
             handleCommandIntent(intent);
         }
 
-        // Make sure the service will shut down on its own if it was
-        // just started but not bound to and nothing is playing
-        scheduleDelayedShutdown();
-
         if (intent != null && intent.getBooleanExtra(FROM_MEDIA_BUTTON, false)) {
             MediaButtonIntentReceiver.completeWakefulIntent(intent);
         }
 
-        return START_NOT_STICKY;
+        // Make sure the service will shut down on its own if it was
+        // just started but not bound to and nothing is playing
+        if (mIsSupposedToBePlaying || mPausedByTransientLossOfFocus) {
+            cancelShutdown();
+            return START_STICKY;
+        } else {
+            scheduleDelayedShutdown();
+            return START_NOT_STICKY;
+        }
     }
 
     private void releaseServiceUiAndStop() {
@@ -868,10 +864,10 @@ public class MusicPlaybackService extends Service {
         mAudioManager.abandonAudioFocus(mAudioFocusListener);
         mSession.setActive(false);
 
-        if (!mServiceInUse) {
+        if (!mIsBound) {
             saveQueue(true);
-            stopSelf(mServiceStartId);
         }
+        stopSelf(mServiceStartId);
     }
 
     private void handleCommandIntent(Intent intent) {
@@ -2409,7 +2405,12 @@ public class MusicPlaybackService extends Service {
             // Update mLastPlayed time first and notify afterwards, as
             // the notification listener method needs the up-to-date value
             // for the recentlyPlayed() method to work
-            if (!mIsSupposedToBePlaying) {
+            if (mIsSupposedToBePlaying) {
+                cancelShutdown();
+                // Make sure we're started explicitly, so that we aren't killed when
+                // the player activity unbinds
+                startForegroundService(new Intent(this, MusicPlaybackService.class));
+            } else {
                 scheduleDelayedShutdown();
                 mLastPlayedTime = System.currentTimeMillis();
             }
@@ -2524,7 +2525,6 @@ public class MusicPlaybackService extends Service {
 
             setIsSupposedToBePlaying(true, true);
 
-            cancelShutdown();
             updateNotification();
         } else if (mPlaylist.size() <= 0) {
             setShuffleMode(SHUFFLE_AUTO);
