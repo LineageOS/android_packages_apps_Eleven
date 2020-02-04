990e72b07bc6733ad92f7295800bb44c157fc618
Danny Baumann <dannybaumann@web.de>
2020-02-04 09:42:27 +0100

    Refactor music playback service binding logic.
    Instead of using a static service reference managed in a distributed
    manner, track binding registrations and fetch service reference from
    registration.
    
    Change-Id: I59cf90b3024cf21310f44954cd7bf5d11e4899a5

--- src/org/lineageos/eleven/MusicPlaybackService.java
+++ src/org/lineageos/eleven/MusicPlaybackService.java
@@ -445,7 +445,7 @@ public class MusicPlaybackService extends Service {
     /**
      * Used to know when the service is active
      */
-    private boolean mServiceInUse = false;
+    private boolean mIsBound = false;
 
     /**
      * Used to know if something should be playing or not
@@ -568,8 +568,7 @@ public class MusicPlaybackService extends Service {
     @Override
     public IBinder onBind(final Intent intent) {
         if (D) Log.d(TAG, "Service bound, intent = " + intent);
-        cancelShutdown();
-        mServiceInUse = true;
+        mIsBound = true;
         return mBinder;
     }
 
@@ -579,7 +578,7 @@ public class MusicPlaybackService extends Service {
     @Override
     public boolean onUnbind(final Intent intent) {
         if (D) Log.d(TAG, "Service unbound");
-        mServiceInUse = false;
+        mIsBound = false;
         saveQueue(true);
 
         if (mReadGranted) {
@@ -594,11 +593,9 @@ public class MusicPlaybackService extends Service {
                 // Also delay stopping the service if we're transitioning between
                 // tracks.
             } else if (mPlaylist.size() > 0 || mPlayerHandler.hasMessages(TRACK_ENDED)) {
-                scheduleDelayedShutdown();
                 return true;
             }
         }
-        stopSelf(mServiceStartId);
 
         return true;
     }
@@ -608,8 +605,7 @@ public class MusicPlaybackService extends Service {
      */
     @Override
     public void onRebind(final Intent intent) {
-        cancelShutdown();
-        mServiceInUse = true;
+        mIsBound = true;
     }
 
     /**
@@ -706,9 +702,6 @@ public class MusicPlaybackService extends Service {
 
         mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         mShutdownIntent = PendingIntent.getService(this, 0, shutdownIntent, 0);
-
-        // Listen for the idle state
-        scheduleDelayedShutdown();
 
         // Bring the queue back
         reloadQueue();
@@ -868,10 +861,10 @@ public class MusicPlaybackService extends Service {
         mAudioManager.abandonAudioFocus(mAudioFocusListener);
         mSession.setActive(false);
 
-        if (!mServiceInUse) {
+        if (!mIsBound) {
             saveQueue(true);
-            stopSelf(mServiceStartId);
         }
+        stopSelf(mServiceStartId);
     }
 
     private void handleCommandIntent(Intent intent) {
@@ -3549,7 +3542,11 @@ public class MusicPlaybackService extends Service {
          */
         @Override
         public void play() throws RemoteException {
-            mService.get().play();
+            MusicPlaybackService service = mService.get();
+            service.play();
+            // We also need to start the service explicitly, as we don't want to stop playback
+            // on unbind in case the service is just started due to being bound
+            service.startForegroundService(new Intent(service, MusicPlaybackService.class));
         }
 
         /**
