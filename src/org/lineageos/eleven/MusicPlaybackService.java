475d186108ad3f91b5f581d4cd6cccfd00961d8f
Danny Baumann <dannybaumann@web.de>
2020-02-07 11:57:49 +0100

    Don't update notification twice on play start.
    It's already updated via
    play()
    -> setIsSupposedToBePlaying()
    -> notifyChange()
    -> updateNotification()
    
    Change-Id: I18ce871b9e56de4ef335e4690c21cce0684ea435

--- src/org/lineageos/eleven/MusicPlaybackService.java
+++ src/org/lineageos/eleven/MusicPlaybackService.java
@@ -2529,8 +2529,6 @@ public class MusicPlaybackService extends Service
             mPlayerHandler.sendEmptyMessage(FADEUP);
 
             setIsSupposedToBePlaying(true, true);
-
-            updateNotification();
         } else if (mPlaylist.size() <= 0) {
             setShuffleMode(SHUFFLE_AUTO);
         }
