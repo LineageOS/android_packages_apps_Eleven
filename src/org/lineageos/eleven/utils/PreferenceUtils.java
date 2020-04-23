2a1486c08de016b5a916fd3713498ee0a02d689c
Bruno Martins <bgcngm@gmail.com>
2020-04-23 13:30:52 +0100

    Eleven: Get rid of lockscreen media art option
    This is controlled globally in lockscreen settings
    and is currently broken since the refactor done in
    commit f93fbfe.
    
    Change-Id: Ieba732a9b4a12d4c69cacc24dfd8e309c32f5bde

--- src/org/lineageos/eleven/utils/PreferenceUtils.java
+++ src/org/lineageos/eleven/utils/PreferenceUtils.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2018-2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven.utils;
@@ -86,9 +91,6 @@ public final class PreferenceUtils {
 
     // shake to play flag
     public static final String SHAKE_TO_PLAY = "shake_to_play";
-
-    // show/hide album art on lockscreen
-    public static final String SHOW_ALBUM_ART_ON_LOCKSCREEN = "lockscreen_album_art";
 
     private static final int PERMISSION_REQUEST_RECORD_AUDIO = 1;
 
@@ -372,9 +374,5 @@ public final class PreferenceUtils {
 
     public boolean getShakeToPlay() {
         return mPreferences.getBoolean(SHAKE_TO_PLAY, false);
-    }
-
-    public boolean getShowAlbumArtOnLockscreen() {
-        return mPreferences.getBoolean(SHOW_ALBUM_ART_ON_LOCKSCREEN, true);
     }
 }
