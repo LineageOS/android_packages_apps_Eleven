efe30260f76c1df838132477fcad94b0f6458ca1
Bruno Martins <bgcngm@gmail.com>
2020-04-24 18:54:08 +0100

    Eleven: Remove dead code
    This code is unused for years and right now Last.fm doesn't even allow
    retrieving artwork through their API (for reference read:
    https://getsatisfaction.com/lastfm/topics/api-announcement-dac8oefw5vrxq)
    
    Change-Id: Iaf5faaa9bc54e3f8ad90d07fded40ba85337de05

--- src/org/lineageos/eleven/Config.java
+++ src/org/lineageos/eleven/Config.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven;
@@ -23,11 +28,6 @@ public final class Config {
     /* This class is never initiated. */
     public Config() {
     }
-
-    /**
-     * My personal Last.fm API key, please use your own.
-     */
-    public static final String LASTFM_API_KEY = "0bec3f7ec1f914d7c960c12a916c8fb3";
 
     /**
      * Used to distinguish album art from artist images
