7ffe145267be4b93adfd8bf1de68901e23f8e30b
Bruno Martins <bgcngm@gmail.com>
2020-04-24 17:17:31 +0100

    Eleven: Remove casts from calls to getSystemService
    Change-Id: I8c14626e2d1c74a4120f511f10f8130065a19529

--- src/org/lineageos/eleven/cache/ImageCache.java
+++ src/org/lineageos/eleven/cache/ImageCache.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2018-2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven.cache;
@@ -184,8 +189,7 @@ public final class ImageCache {
      * @param context The {@link Context} to use
      */
     public void initLruCache(final Context context) {
-        final ActivityManager activityManager = (ActivityManager)context
-                .getSystemService(Context.ACTIVITY_SERVICE);
+        final ActivityManager activityManager = context.getSystemService(ActivityManager.class);
         final int lruCacheSize = Math.round(MEM_CACHE_DIVIDER * activityManager.getMemoryClass()
                 * 1024 * 1024);
         mLruCache = new MemoryCache(lruCacheSize);
