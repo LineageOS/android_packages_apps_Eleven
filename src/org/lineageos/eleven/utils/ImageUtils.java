36cb44f0330abd229ae392f7d33187f252277d79
Bruno Martins <bgcngm@gmail.com>
2020-04-24 18:54:08 +0100

    Eleven: Remove dead code
    This code is unused for years and right now Last.fm doesn't even allow
    retrieving artwork through their API (for reference read:
    https://getsatisfaction.com/lastfm/topics/api-announcement-dac8oefw5vrxq)
    
    Change-Id: Iaf5faaa9bc54e3f8ad90d07fded40ba85337de05

--- src/org/lineageos/eleven/utils/ImageUtils.java
+++ src/org/lineageos/eleven/utils/ImageUtils.java
@@ -15,130 +15,14 @@
 */
 package org.lineageos.eleven.utils;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.widget.ImageView;
 
-import org.lineageos.eleven.cache.ImageCache;
-import org.lineageos.eleven.cache.ImageWorker;
-import org.lineageos.eleven.lastfm.ImageSize;
-import org.lineageos.eleven.lastfm.MusicEntry;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class ImageUtils {
-    private static final String DEFAULT_HTTP_CACHE_DIR = "http"; //$NON-NLS-1$
-
-    public static final int IO_BUFFER_SIZE_BYTES = 1024;
-
     private static final int DEFAULT_MAX_IMAGE_HEIGHT = 1024;
 
     private static final int DEFAULT_MAX_IMAGE_WIDTH = 1024;
-
-    private static AtomicInteger sInteger = new AtomicInteger(0);
-
-    /**
-     * Gets the image url based on the imageType
-     * @param artistName The artist name param used in the Last.fm API.
-     * @param albumName The album name param used in the Last.fm API.
-     * @param imageType The type of image URL to fetch for.
-     * @return The image URL for an artist image or album image.
-     */
-    public static String processImageUrl(final Context context, final String artistName,
-                                         final String albumName, final ImageWorker.ImageType imageType) {
-        switch (imageType) {
-            case ARTIST:
-                // Disable last.fm calls - TODO: Find an alternative artwork provider that has
-                // the proper license rights for artwork
-                /*if (!TextUtils.isEmpty(artistName)) {
-                    if (PreferenceUtils.getInstance(context).downloadMissingArtistImages()) {
-                        final Artist artist = Artist.getInfo(context, artistName);
-                        if (artist != null) {
-                            return getBestImage(artist);
-                        }
-                    }
-                }*/
-                break;
-            case ALBUM:
-                // Disable last.fm calls - TODO: Find an alternative artwork provider that has
-                // the proper license rights for artwork
-                /*if (!TextUtils.isEmpty(artistName) && !TextUtils.isEmpty(albumName)) {
-                    if (PreferenceUtils.getInstance(context).downloadMissingArtwork()) {
-                        final Artist correction = Artist.getCorrection(context, artistName);
-                        if (correction != null) {
-                            final Album album = Album.getInfo(context, correction.getName(),
-                                    albumName);
-                            if (album != null) {
-                                return getBestImage(album);
-                            }
-                        }
-                    }
-                }*/
-                break;
-            default:
-                break;
-        }
-        return null;
-    }
-
-    /**
-     * Downloads the bitmap from the url and returns it after some processing
-     *
-     * @param key The key to identify which image to process, as provided by
-     *            {@link ImageWorker#loadImage(mKey, android.widget.ImageView)}
-     * @return The processed {@link Bitmap}.
-     */
-    public static Bitmap processBitmap(final Context context, final String url) {
-        if (url == null) {
-            return null;
-        }
-        final File file = downloadBitmapToFile(context, url, DEFAULT_HTTP_CACHE_DIR);
-        if (file != null) {
-            // Return a sampled down version
-            final Bitmap bitmap = decodeSampledBitmapFromFile(file.toString());
-            file.delete();
-            if (bitmap != null) {
-                return bitmap;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Decode and sample down a {@link Bitmap} from a file to the requested
-     * width and height.
-     *
-     * @param filename The full path of the file to decode
-     * @param reqWidth The requested width of the resulting bitmap
-     * @param reqHeight The requested height of the resulting bitmap
-     * @return A {@link Bitmap} sampled down from the original with the same
-     *         aspect ratio and dimensions that are equal to or greater than the
-     *         requested width and height
-     */
-    public static Bitmap decodeSampledBitmapFromFile(final String filename) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filename, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, DEFAULT_MAX_IMAGE_WIDTH,
-                DEFAULT_MAX_IMAGE_HEIGHT);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filename, options);
-    }
 
     /**
      * Calculate an inSampleSize for use in a
@@ -188,83 +72,6 @@ public class ImageUtils {
             }
         }
         return inSampleSize;
-    }
-
-    private static String getBestImage(MusicEntry e) {
-        final ImageSize[] QUALITY = {ImageSize.EXTRALARGE, ImageSize.LARGE, ImageSize.MEDIUM,
-                ImageSize.SMALL, ImageSize.UNKNOWN};
-        for(ImageSize q : QUALITY) {
-            String url = e.getImageURL(q);
-            if (url != null) {
-                return url;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Download a {@link Bitmap} from a URL, write it to a disk and return the
-     * File pointer. This implementation uses a simple disk cache.
-     *
-     * @param context The context to use
-     * @param urlString The URL to fetch
-     * @return A {@link File} pointing to the fetched bitmap
-     */
-    public static final File downloadBitmapToFile(final Context context, final String urlString,
-                                                  final String uniqueName) {
-        final File cacheDir = ImageCache.getDiskCacheDir(context, uniqueName);
-
-        if (!cacheDir.exists()) {
-            cacheDir.mkdir();
-        }
-
-        HttpURLConnection urlConnection = null;
-        BufferedOutputStream out = null;
-
-        try {
-            // increment the number to not collisions on the temp file name.  A collision can
-            // potentially cause up to 50s on the first creation of the temp file but not on
-            // subsequent ones for some reason.
-            int number = sInteger.getAndIncrement() % 10;
-            final File tempFile = File.createTempFile("bitmap" + number, null, cacheDir); //$NON-NLS-1$
-
-            final URL url = new URL(urlString);
-            urlConnection = (HttpURLConnection)url.openConnection();
-            if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                return null;
-            }
-            int contentLength = urlConnection.getContentLength();
-            final InputStream in = new BufferedInputStream(urlConnection.getInputStream(),
-                    IO_BUFFER_SIZE_BYTES);
-            out = new BufferedOutputStream(new FileOutputStream(tempFile), IO_BUFFER_SIZE_BYTES);
-
-            final byte[] buffer = new byte[IO_BUFFER_SIZE_BYTES];
-            int numBytes;
-            while ((numBytes = in.read(buffer)) != -1) {
-                out.write(buffer, 0, numBytes);
-                contentLength -= numBytes;
-            }
-
-            // valid values for contentLength are either -ve (meaning it wasn't set) or 0
-            // if it is  > 0 that means we got a value but didn't fully download the content
-            if (contentLength > 0) {
-                return null;
-            }
-
-            return tempFile;
-        } catch (final IOException ignored) {
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (final IOException ignored) {
-                }
-            }
-        }
-        return null;
     }
 
     /**
