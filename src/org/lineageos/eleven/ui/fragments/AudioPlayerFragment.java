6756ff7c5dbd295313268ef91bc60bead3db6e9d
DJABhipHop <castellanos.kevin@rocketmail.com>
2020-04-27 22:26:43 -0400

    Eleven: redesigning active player fragment layout.
    Making active player fragment more useful .
    Making portrait mode look nice.
    Making landscape mode match the new portrait mode layout.
    Making layout more tablet friendly.
    
    Change-Id: I7dcb27ece39db1cb542e80f1f333bf9204ea7680

--- src/org/lineageos/eleven/ui/fragments/AudioPlayerFragment.java
+++ src/org/lineageos/eleven/ui/fragments/AudioPlayerFragment.java
@@ -326,8 +326,8 @@ public class AudioPlayerFragment extends Fragment implements ServiceConnection {
         mPlayerToolBar = mRootView.findViewById(R.id.audio_player_header);
 
         // Title text
-        mSongTitle = mRootView.findViewById(R.id.header_bar_song_title);
-        mArtistName = mRootView.findViewById(R.id.header_bar_artist_title);
+        mSongTitle = mRootView.findViewById(R.id.main_info_flow_song_title);
+        mArtistName = mRootView.findViewById(R.id.main_info_flow_artist_title);
     }
 
     /**
