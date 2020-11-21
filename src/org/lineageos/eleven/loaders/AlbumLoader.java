14e01b8fb650e9dbabb0b6c1e453c0294f3b35a3
Craig G <kraigs.android@gmail.com>
2020-11-21 11:32:19 -0500

    Eleven: Fix album id column
    The derived media databases use multiple explicit names for the columns that
    contain unique ids from their parent table, not a single _id column.  This
    change explicitly selects the album unique id when loading an album.
    
    Caused by: java.lang.IllegalArgumentException: Invalid column _id
    	at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:170)
    	at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:140)
    	at android.content.ContentProviderProxy.query(ContentProviderNative.java:423)
    	at android.content.ContentResolver.query(ContentResolver.java:944)
    	at android.content.ContentResolver.query(ContentResolver.java:880)
    	at android.content.ContentResolver.query(ContentResolver.java:836)
    	at org.lineageos.eleven.loaders.AlbumLoader.makeAlbumCursor(AlbumLoader.java:150)
    	at org.lineageos.eleven.loaders.AlbumLoader.loadInBackground(AlbumLoader.java:76)
    	at org.lineageos.eleven.loaders.AlbumLoader.loadInBackground(AlbumLoader.java:41)
    	...
    
    Change-Id: Id2bffacb97413228a4f8871132206503900a2a83

--- src/org/lineageos/eleven/loaders/AlbumLoader.java
+++ src/org/lineageos/eleven/loaders/AlbumLoader.java
@@ -1,14 +1,19 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Copyright (C) 2014 The CyanogenMod Project
- * Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.eleven.loaders;
@@ -150,7 +155,7 @@ public class AlbumLoader extends SectionCreator.SimpleListLoader<Album> {
         Cursor cursor = context.getContentResolver().query(uri,
                 new String[] {
                         /* 0 */
-                        BaseColumns._ID,
+                        AlbumColumns.ALBUM_ID,
                         /* 1 */
                         AlbumColumns.ALBUM,
                         /* 2 */
@@ -165,8 +170,9 @@ public class AlbumLoader extends SectionCreator.SimpleListLoader<Album> {
         final SortParameter sortParameter = getSortParameter(albumSortOrder);
         if (sortParameter != null && cursor != null) {
             final boolean descending = MusicUtils.isSortOrderDesending(albumSortOrder);
-            return LocalizedStore.getInstance(context).getLocalizedSort(cursor, BaseColumns._ID,
-                    SortParameter.Album, sortParameter, descending, artistId == null);
+            return LocalizedStore.getInstance(context).getLocalizedSort(cursor,
+                    AlbumColumns.ALBUM_ID, SortParameter.Album, sortParameter,
+                    descending, artistId == null);
         }
 
         return cursor;
