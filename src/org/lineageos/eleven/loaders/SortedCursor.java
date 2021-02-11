7edb69c01992a3894f11543f906c02926dbceb43
Joey <joey@lineageos.org>
2021-02-11 10:54:15 +0100

    Eleven: the great cleanup
    - Remove all java dead code
    - Update deprecated methods / classes calls
    - Kill all the {@inheritdoc}s
    - Minor code style fixes (fields -> local variables, final, indentation...)
    - Make use of newer java lang features (lambdas, try catch resources...)
    - Update copyrights to be consistent
    
    Signed-off-by: Joey <joey@lineageos.org>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: Ic9e3bbd3e82fa4ea2e87e683a99340fba4974beb

--- src/org/lineageos/eleven/loaders/SortedCursor.java
+++ src/org/lineageos/eleven/loaders/SortedCursor.java
@@ -1,18 +1,19 @@
 /*
-* Copyright (C) 2014 The CyanogenMod Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.lineageos.eleven.loaders;
 
 import android.database.AbstractCursor;
@@ -34,19 +35,19 @@ public class SortedCursor extends AbstractCursor {
     // the map of external indices to internal indices
     private ArrayList<Integer> mOrderedPositions;
     // this contains the ids that weren't found in the underlying cursor
-    private ArrayList<Long> mMissingIds;
+    private final ArrayList<Long> mMissingIds;
     // this contains the mapped cursor positions and afterwards the extra ids that weren't found
     private HashMap<Long, Integer> mMapCursorPositions;
     // extra we want to store with the cursor
     private ArrayList<Object> mExtraData;
 
     /**
-     * @param cursor to wrap
-     * @param order the list of unique ids in sorted order to display
+     * @param cursor     to wrap
+     * @param order      the list of unique ids in sorted order to display
      * @param columnName the column name of the id to look up in the internal cursor
      */
     public SortedCursor(final Cursor cursor, final long[] order, final String columnName,
-            final List<? extends Object> extraData) {
+                        final List<? extends Object> extraData) {
         if (cursor == null) {
             throw new IllegalArgumentException("Non-null cursor is needed");
         }
@@ -58,12 +59,13 @@ public class SortedCursor extends AbstractCursor {
     /**
      * This function populates mOrderedPositions with the cursor positions in the order based
      * on the order passed in
-     * @param order the target order of the internal cursor
+     *
+     * @param order     the target order of the internal cursor
      * @param extraData Extra data we want to add to the cursor
      * @return returns the ids that aren't found in the underlying cursor
      */
-    private ArrayList<Long> buildCursorPositionMapping(final long[] order,
-            final String columnName, final List<? extends Object> extraData) {
+    private ArrayList<Long> buildCursorPositionMapping(final long[] order, final String columnName,
+                                                       final List<?> extraData) {
         ArrayList<Long> missingIds = new ArrayList<>();
 
         mOrderedPositions = new ArrayList<>(mCursor.getCount());
