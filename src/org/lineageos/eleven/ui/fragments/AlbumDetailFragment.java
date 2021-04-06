19d01e9138fada762d8b396fb7095a6847504bb2
2bllw8 <2bllw8@gmail.com>
2021-04-06 14:28:55 +0200

    Eleven: convert albums and artist lists to RecyclerView
    Change-Id: I55e0a82a77725039ea3c6639d7cd408aa119304f

--- src/org/lineageos/eleven/ui/fragments/AlbumDetailFragment.java
+++ src/org/lineageos/eleven/ui/fragments/AlbumDetailFragment.java
@@ -17,13 +17,19 @@
 package org.lineageos.eleven.ui.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.lineageos.eleven.Config;
 import org.lineageos.eleven.R;
@@ -31,6 +37,7 @@ import org.lineageos.eleven.adapters.AlbumDetailSongAdapter;
 import org.lineageos.eleven.adapters.DetailSongAdapter;
 import org.lineageos.eleven.adapters.PagerAdapter;
 import org.lineageos.eleven.cache.ImageFetcher;
+import org.lineageos.eleven.loaders.AlbumSongLoader;
 import org.lineageos.eleven.model.Album;
 import org.lineageos.eleven.model.Song;
 import org.lineageos.eleven.utils.AlbumPopupMenuHelper;
@@ -42,7 +49,8 @@ import org.lineageos.eleven.widgets.LoadingEmptyContainer;
 
 import java.util.List;
 
-public class AlbumDetailFragment extends DetailFragment implements IChildFragment {
+public class AlbumDetailFragment extends DetailFragment implements IChildFragment,
+        LoaderManager.LoaderCallbacks<List<Song>> {
     private static final int LOADER_ID = 1;
 
     private DetailSongAdapter mSongAdapter;
@@ -77,7 +85,7 @@ public class AlbumDetailFragment extends DetailFragment implements IChildFragmen
         setupHeader(artistName, args);
         setupSongList();
 
-        LoaderManager.getInstance(this).initLoader(LOADER_ID, args, mSongAdapter);
+        LoaderManager.getInstance(this).initLoader(LOADER_ID, args, this);
     }
 
     @Override // DetailFragment
@@ -159,24 +167,15 @@ public class AlbumDetailFragment extends DetailFragment implements IChildFragmen
     }
 
     private void setupSongList() {
-        ListView songsList = mRootView.findViewById(R.id.songs);
-        mSongAdapter = new AlbumDetailSongAdapter(getActivity(), this) {
-            @Override
-            protected void onLoading() {
-                mLoadingEmptyContainer.showLoading();
-            }
-
-            @Override
-            protected void onNoResults() {
-                getContainingActivity().postRemoveFragment(AlbumDetailFragment.this);
-            }
-        };
+        RecyclerView songsList = mRootView.findViewById(R.id.songs);
+        mSongAdapter = new AlbumDetailSongAdapter(getActivity());
         mSongAdapter.setPopupMenuClickedListener((v, position) ->
                 mSongMenuHelper.showPopupMenu(v, position));
+        songsList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        songsList.setItemAnimator(new DefaultItemAnimator());
         songsList.setAdapter(mSongAdapter);
-        songsList.setOnItemClickListener(mSongAdapter);
         mLoadingEmptyContainer = mRootView.findViewById(R.id.loading_empty_container);
-        songsList.setEmptyView(mLoadingEmptyContainer);
+        mLoadingEmptyContainer.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -199,7 +198,7 @@ public class AlbumDetailFragment extends DetailFragment implements IChildFragmen
 
     @Override
     public void restartLoader() {
-        LoaderManager.getInstance(this).restartLoader(LOADER_ID, getArguments(), mSongAdapter);
+        LoaderManager.getInstance(this).restartLoader(LOADER_ID, getArguments(), this);
         ImageFetcher.getInstance(getActivity()).loadAlbumImage(mArtistName, mAlbumName, mAlbumId,
                 mAlbumArt);
     }
@@ -214,5 +213,35 @@ public class AlbumDetailFragment extends DetailFragment implements IChildFragmen
     @Override
     public PagerAdapter.MusicFragments getMusicFragmentParent() {
         return PagerAdapter.MusicFragments.ALBUM;
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<Song>> onCreateLoader(int id, @Nullable Bundle args) {
+        mLoadingEmptyContainer.showLoading();
+        long sourceId = args == null ? -1 : args.getLong(Config.ID);
+        mSongAdapter.setSourceId(sourceId);
+        return new AlbumSongLoader(getContext(), sourceId);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Song>> loader, List<Song> data) {
+        Handler handler = new Handler(requireActivity().getMainLooper());
+
+        if (data.isEmpty()) {
+            getContainingActivity().postRemoveFragment(AlbumDetailFragment.this);
+            return;
+        }
+
+        mLoadingEmptyContainer.setVisibility(View.GONE);
+        // Do on UI thread: https://issuetracker.google.com/issues/37030377
+        handler.post(() -> mSongAdapter.setData(data));
+        update(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Song>> loader) {
+        // Clear the data in the adapter
+        mSongAdapter.unload();
     }
 }
