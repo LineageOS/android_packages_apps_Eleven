d57bf088f8c52b59582c6afaa2e36257296ea97f
2bllw8 <2bllw8@gmail.com>
2021-04-07 12:05:32 +0200

    Eleven: convert queue list to RecyclerView
    Change-Id: Ifba5e797619d3c6a9506660adab36f08fd71006d

---
+++ src/org/lineageos/eleven/widgets/DragSortItemTouchHelperCallback.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.eleven.widgets;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DragSortItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
+    private final DragSortListener mListener;
+
+    public DragSortItemTouchHelperCallback(DragSortListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView,
+                                @NonNull RecyclerView.ViewHolder viewHolder) {
+        return makeMovementFlags(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView,
+                          @NonNull RecyclerView.ViewHolder source,
+                          @NonNull RecyclerView.ViewHolder target) {
+        if (source.getItemViewType() != target.getItemViewType()) {
+            return false;
+        }
+
+        mListener.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+}
