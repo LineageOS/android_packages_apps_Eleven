8792159fa9ae60be43e4d13f1390a387f29a82dd
Michael Bestas <mkbestas@gmail.com>
2021-05-27 00:11:15 +0300

    Eleven: Remove unused imports
    Change-Id: Ib0a01141795c25fb8ad9e9bf208243707c4334a7

--- src/org/lineageos/eleven/adapters/AlbumDetailSongAdapter.java
+++ src/org/lineageos/eleven/adapters/AlbumDetailSongAdapter.java
@@ -17,23 +17,16 @@
 package org.lineageos.eleven.adapters;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
-import androidx.loader.content.Loader;
 
 import org.lineageos.eleven.Config;
 import org.lineageos.eleven.R;
 import org.lineageos.eleven.cache.ImageFetcher;
-import org.lineageos.eleven.loaders.AlbumSongLoader;
 import org.lineageos.eleven.model.Song;
-import org.lineageos.eleven.ui.fragments.AlbumDetailFragment;
 import org.lineageos.eleven.utils.MusicUtils;
-
-import java.util.List;
 
 public class AlbumDetailSongAdapter extends DetailSongAdapter {
 
