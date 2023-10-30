ba516bd9d6264e2cbba762febe0a64fb5faa82fc
Georg Veichtlbauer <georg@vware.at>
2023-10-30 13:35:43 +0100

    Eleven: Migrate to from Groovy to Kotlin DSL
    Change-Id: I122db6df302b41c0e8861e191a340b3bca7e3a98

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Eleven"
