ff17b77d75bddb410cd56d3dc4f96e721e50943e
Jake Florence <retromac9x2@gmail.com>
2023-10-31 12:15:12 +0000

    Eleven: Update Compile SDK and Target SDK to 33
    also update Gradle, dependencies and some permissions so it will work
    
    Change-Id: I2c06121f17e9074de60a70417cdbe1d800a5bd8f

--- src/org/lineageos/eleven/ui/activities/HomeActivity.java
+++ src/org/lineageos/eleven/ui/activities/HomeActivity.java
@@ -27,6 +27,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -36,7 +37,10 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.ColorUtils;
@@ -96,15 +100,52 @@ public class HomeActivity extends SlidingPanelActivity implements
      */
     protected boolean mTopLevelActivity = false;
 
+    public void denied_permission() {
+        Toast.makeText(getApplicationContext(), R.string.request_perms, Toast.LENGTH_SHORT).show();
+        // You can directly ask for the permission.
+        // The registered ActivityResultCallback gets the result of this request.
+        requestPermissionLauncher.launch(
+                Manifest.permission.READ_MEDIA_AUDIO);
+    }
+
+    // Register the permissions callback, which handles the user's response to the
+// system permissions dialog. Save the return value, an instance of
+// ActivityResultLauncher, as an instance variable.
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    if (!needRequestStoragePermission()) {
+                        init(mSavedInstanceState);
+                    }
+                } else {
+                    denied_permission();
+                }
+            });
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         mSavedInstanceState = savedInstanceState;
         mRootView = getWindow().getDecorView();
-
-        if (!needRequestStoragePermission()) {
-            init(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.READ_MEDIA_AUDIO) ==
+                PackageManager.PERMISSION_GRANTED) {
+                if (!needRequestStoragePermission()) {
+                    init(savedInstanceState);
+                }
+            } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                        Manifest.permission.READ_MEDIA_AUDIO);
+            }
+        } else {
+            if (!needRequestStoragePermission()) {
+                init(savedInstanceState);
+            }
         }
     }
 
